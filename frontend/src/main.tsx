83021fa8b0d4dd180cf39b06f2f39e7dceff9937
Lovely <lovelysharmacji@gmail.com>
2025-09-09 18:40:43 +0530

    clientid added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 
-  const clientId = "919925336890-t87hkk814ljl9r660p1ink3mi98h4ogd.apps.googleusercontent.com";
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
